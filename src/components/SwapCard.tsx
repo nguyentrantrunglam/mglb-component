8fa519a47b6a4c4ec7bde65fd6d5b33388078ba2
MHTrungLam <lamntt@mor.com.vn>
2023-04-06 02:48:50 +0700

    SwapCard

--- src/components/SwapCard.tsx
+++ src/components/SwapCard.tsx
@@ -22,12 +22,30 @@ export function SwapCard({}: Props) {
       title: "Card 3",
       status: "right",
     },
-    // {
-    //   id: 4,
-    //   url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
-    //   title: "Card 3",
-    //   status: "behind",
-    // },
+    {
+      id: 4,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
+      title: "Card 4",
+      status: "behind",
+    },
+    {
+      id: 5,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/299332500_3289138938037810_5543890420180190757_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=P2A74sF581cAX-7WX3Y&_nc_ht=scontent.fhan2-4.fna&oh=00_AfBZKSHCy-0kyMjB4ypj4wsdT2nybwPB_Mq4nSpJyhbV4g&oe=6431D4BD",
+      title: "Card 5",
+      status: "behind",
+    },
+    {
+      id: 6,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
+      title: "Card 6",
+      status: "behind",
+    },
+    {
+      id: 7,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
+      title: "Card 7",
+      status: "behind",
+    },
   ];
   const [listCards, setListCards] = React.useState(cards);
   const handleClick = (clickIndex: number) => {
@@ -50,17 +68,42 @@ export function SwapCard({}: Props) {
     });
     setListCards(newCards);
   };
+  const handleClickButton = (clickIndex: number) => {
+    const activeIndex = listCards.findIndex((card) => card.status == "active");
+    if (activeIndex == clickIndex) return;
+    else {
+      if (activeIndex < clickIndex) {
+        handleClick(activeIndex + 1);
+        setTimeout(() => {
+          handleClickButton(clickIndex);
+        }, 800);
+      } else {
+        handleClick(activeIndex - 1);
+        setTimeout(() => {
+          handleClickButton(clickIndex);
+        }, 800);
+      }
+    }
+  };
   return (
-    <div className="swap-cards">
-      {listCards.map((card, index) => (
-        <div
-          key={card.id}
-          className={`card ${card.status}`}
-          onClick={() => handleClick(index)}
-        >
-          <img src={card.url} className="card-image" />
-        </div>
-      ))}
+    <div className="container">
+      <div className="swap-cards">
+        {listCards.map((card, index) => (
+          <div
+            key={card.id}
+            className={`card ${card.status}`}
+            onClick={() => handleClick(index)}
+          >
+            <img src={card.url} className="card-image" />
+            <div className="card-name">{card.title}</div>
+          </div>
+        ))}
+      </div>
+      <div className="list-buttons">
+        {listCards.map((card, index) => (
+          <button onClick={() => handleClickButton(index)}>{card.title}</button>
+        ))}
+      </div>
     </div>
   );
 }
