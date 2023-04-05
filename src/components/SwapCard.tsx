f085c1d5806a1dbd0f6f3e007b11513547f3d67f
MHTrungLam <lamntt@mor.com.vn>
2023-04-06 02:20:57 +0700

    SwapCard

---
+++ src/components/SwapCard.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./SwapCard.scss";
+type Props = {};
+
+export function SwapCard({}: Props) {
+  const cards = [
+    {
+      id: 1,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/334563997_1427069548031427_6153761672655437636_n.jpg?stp=cp6_dst-jpg&_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=u-Q_p2n09UoAX8PYxl6&_nc_ht=scontent.fhan2-4.fna&oh=00_AfD5FUJS64NWp0Q8MFdVLOQ2ANfOW6b3Y2FCfgKZSRn5Aw&oe=64327769",
+      title: "Card 1",
+      status: "left",
+    },
+    {
+      id: 2,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/299332500_3289138938037810_5543890420180190757_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=P2A74sF581cAX-7WX3Y&_nc_ht=scontent.fhan2-4.fna&oh=00_AfBZKSHCy-0kyMjB4ypj4wsdT2nybwPB_Mq4nSpJyhbV4g&oe=6431D4BD",
+      title: "Card 2",
+      status: "active",
+    },
+    {
+      id: 3,
+      url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
+      title: "Card 3",
+      status: "right",
+    },
+    // {
+    //   id: 4,
+    //   url: "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.30808-6/275138739_3164872860464419_6861463209642361691_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=qtgl7hmih4YAX-nk8CS&_nc_ht=scontent.fhan2-4.fna&oh=00_AfCLP3VpT38mIlmtA_aeFm-fucz_jf4jDpgpBYX4L7gUfg&oe=64339B2B",
+    //   title: "Card 3",
+    //   status: "behind",
+    // },
+  ];
+  const [listCards, setListCards] = React.useState(cards);
+  const handleClick = (clickIndex: number) => {
+    let newCards = [...listCards];
+    newCards.forEach((card, index) => {
+      card.status = "behind";
+      if (clickIndex == 0) {
+        newCards[clickIndex].status = "active";
+        newCards[clickIndex + 1].status = "right";
+        newCards[newCards.length - 1].status = "left";
+      } else if (clickIndex == newCards.length - 1) {
+        newCards[clickIndex].status = "active";
+        newCards[clickIndex - 1].status = "left";
+        newCards[0].status = "right";
+      } else {
+        newCards[clickIndex].status = "active";
+        newCards[clickIndex - 1].status = "left";
+        newCards[clickIndex + 1].status = "right";
+      }
+    });
+    setListCards(newCards);
+  };
+  return (
+    <div className="swap-cards">
+      {listCards.map((card, index) => (
+        <div
+          key={card.id}
+          className={`card ${card.status}`}
+          onClick={() => handleClick(index)}
+        >
+          <img src={card.url} className="card-image" />
+        </div>
+      ))}
+    </div>
+  );
+}
